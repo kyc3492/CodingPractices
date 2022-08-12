40462f22521cc46e2ecfed95b766a6bc50734651
kyc3492 <kyc3492@gmail.com>
2022-08-12 23:02:17 +0900

    solved 10844

---
+++ Bakjoon-SWEA/Python/Dynamic_Programming/10844.py
@@ -0,0 +1,19 @@
+N = int(input())
+answer = 0
+dp = [[0] * 10 for _ in range(N)]
+for i in range(1, 10):
+    dp[0][i] = 1
+
+for i in range(1, N):
+    for j in range(10):
+        if j == 0:
+            dp[i][0] = dp[i - 1][1]
+        elif j == 9:
+            dp[i][9] = dp[i - 1][8]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+
+#print(dp)
+answer = sum(dp[N - 1])
+
+print(answer % 1000000000)
