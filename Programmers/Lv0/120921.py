2fe8ea684ae3ea63e72362e600a9d7f2ac8a5e95
kyc3492 <kyc3492@gmail.com>
2023-02-22 00:06:56 +0900

    modded Programmers Lv0 problem

--- Programmers/Lv0/120921.py
+++ Programmers/Lv0/120921.py
@@ -1,42 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=python3
 # 문자열 밀기. deque로 풀면 더 빠를듯?
 
+from collections import deque
+
 def rotate_right(A, B):
     cnt = 0
-    A = list(A)
-    B = list(B)
+    A = deque(A)
+    B = deque(B)
     while (A != B):
         if cnt == len(A):
             break
-        tmp = A[-1]
-        tmp_list = list(A[0:-1])
-        A = []
-        A.append(tmp)
-        A.extend(tmp_list)
-        cnt += 1
-        print(A, cnt)
-    return cnt
-    
-def rotate_left(A, B):
-    cnt = 0
-    A = list(A)
-    B = list(B)
-    while (A != B or cnt < len(A)):
-        if cnt == len(A):
-            break
-        tmp = A[0]
-        A = list(A[1:])
-        A.append(tmp)
+        tmp = A.pop()
+        A.appendleft(tmp)
         cnt += 1
         print(A, cnt)
     return cnt
 
 def solution(A, B):
-    answer = 0
-    
-    cnt_right = rotate_right(A, B)
-    cnt_left = rotate_left(A, B)
-    answer = min(cnt_right, cnt_left)
+    answer = rotate_right(A, B)
     if answer == len(A):
         answer = -1
     
